983bf7afea956790e9be7e0a09db000cbcbd83e0
Philipp Hansch <dev@phansch.net>
2019-12-30 22:21:45 +0100

    Improve tmux session attachment

--- src/tmux_git.rs
+++ src/tmux_git.rs
@@ -1,8 +1,11 @@
 use std::fmt;
 use std::process::Command;
 
-use tmux_interface::{NewSession, SwitchClient};
+use tmux_interface::{AttachSession, NewSession, SwitchClient};
 use tmux_interface::TmuxInterface;
+
+use std::env::VarError;
+use std::env;
 
 pub struct TaskError<'a> {
     status: usize,
@@ -16,15 +19,64 @@ impl<'a> fmt::Display for TaskError<'a> {
     }
 }
 
+fn attach_session(task_name: &str) -> Result<(), TaskError> {
+    println!("AttachSession");
+    let tmux = TmuxInterface::new();
+
+    let attach = AttachSession {
+        target_session: Some(task_name),
+        detach_other: Some(false),
+        cwd: None,
+        not_update_env: Some(false),
+        read_only: Some(false)
+    };
+    match tmux.attach_session(&attach) {
+        Ok(output) => {
+            if !output.status.success() {
+                return Err(
+                    TaskError {
+                        status: output.status.code().unwrap_or(1) as usize,
+                        message: std::str::from_utf8(&output.stderr).unwrap().trim().to_string(),
+                        source: "tmux"
+                    }
+                );
+            }
+        },
+        Err(e) => {
+            return Err(TaskError { status: e.err_type, message: e.err_text, source: "tmux" });
+        }
+    }
+    Ok(())
+}
+
 pub fn focus_tmux_session_and_branch(task_name: &str) -> Result<(), TaskError> {
     let tmux = TmuxInterface::new();
 
-    let switch = SwitchClient {
-        target_session: Some(task_name),
-        ..Default::default()
-    };
-    if let Err(e) = tmux.switch_client(&switch) {
-        return Err(TaskError { status: e.err_type, message: e.err_text, source: "tmux" });
+    match env::var("TMUX") {
+        Err(VarError::NotPresent) => {
+            // If we're not connected to a tmux session, we have to attach first.
+            // (We can't switch to it)
+
+            return attach_session(task_name);
+        },
+        Err(VarError::NotUnicode(_)) => {
+            eprintln!("Bailing, TMUX env var contains non-Unicode characters");
+            std::process::exit(1);
+        },
+        Ok(val) => {
+            if val.is_empty() {
+                return attach_session(task_name);
+            } else {
+                println!("SwitchClient");
+                let switch = SwitchClient {
+                    target_session: Some(task_name),
+                    ..Default::default()
+                };
+                if let Err(e) = tmux.switch_client(&switch) {
+                    return Err(TaskError { status: e.err_type, message: e.err_text, source: "tmux" });
+                }
+            }
+        }
     }
     Ok(())
 }
